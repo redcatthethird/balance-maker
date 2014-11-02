deb2879a165580aa306da1ec0d38be0787b8520f
Red Cat <cred@gmx.us>
2014-11-02 01:48:17 +0000

    Implemented initial text field validation.

--- src/balancemaker/UI/AddTransactionDialog.java
+++ src/balancemaker/UI/AddTransactionDialog.java
@@ -5,11 +5,15 @@
  */
 package balancemaker.UI;
 
-import balancemaker.Buyer;
-import balancemaker.Manager;
-import balancemaker.Transaction;
+import balancemaker.*;
+import java.awt.Color;
 import java.time.Instant;
 import java.util.Date;
+import javax.swing.BorderFactory;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import org.jdesktop.xswingx.PromptSupport;
 
 /**
@@ -17,9 +21,15 @@ import org.jdesktop.xswingx.PromptSupport;
  * @author Andu
  */
 public class AddTransactionDialog extends javax.swing.JDialog {
+    
+    private final Border defaultTextFieldBorder = new JTextField().getBorder();
+    private final Border invalidTextFieldBorder = BorderFactory.createCompoundBorder(
+                    BorderFactory.createLineBorder(Color.RED, 2), defaultTextFieldBorder);
 
     /**
      * Creates new form AddTransactionDialog
+     * @param parent
+     * @param modal
      */
     public AddTransactionDialog(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
@@ -31,23 +41,22 @@ public class AddTransactionDialog extends javax.swing.JDialog {
         PromptSupport.setFocusBehavior(PromptSupport.FocusBehavior.SHOW_PROMPT, receiptTextBox);
         PromptSupport.setPrompt("Enter amount", amountTextBox);
         PromptSupport.setFocusBehavior(PromptSupport.FocusBehavior.SHOW_PROMPT, amountTextBox);
+        
+        validateStore();
+        validateAmount();
+        validateReceipt();
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
         storeTextBox = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
         receiptTextBox = new javax.swing.JTextField();
         amountTextBox = new javax.swing.JTextField();
-        jLabel4 = new javax.swing.JLabel();
         buyerList = new javax.swing.JComboBox();
-        jLabel5 = new javax.swing.JLabel();
         paybackCheckBox = new javax.swing.JCheckBox();
-        jLabel7 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         saveButton = new javax.swing.JButton();
         cancelButton = new javax.swing.JButton();
@@ -56,21 +65,29 @@ public class AddTransactionDialog extends javax.swing.JDialog {
         setTitle("Add transaction");
         setMinimumSize(new java.awt.Dimension(300, 316));
 
-        jLabel1.setText("Store :");
+        storeTextBox.getDocument().addDocumentListener(new DocumentListener() {
+            public void changedUpdate(DocumentEvent e) { validateStore(); }
+            public void removeUpdate(DocumentEvent e) { validateStore(); }
+            public void insertUpdate(DocumentEvent e) { validateStore(); }
+        });
 
         jLabel2.setText("Date :");
 
-        jLabel3.setText("Receipt :");
+        receiptTextBox.getDocument().addDocumentListener(new DocumentListener() {
+            public void changedUpdate(DocumentEvent e) { validateReceipt(); }
+            public void removeUpdate(DocumentEvent e) { validateReceipt(); }
+            public void insertUpdate(DocumentEvent e) { validateReceipt(); }
+        });
 
-        jLabel4.setText("Buyer :");
+        amountTextBox.getDocument().addDocumentListener(new DocumentListener() {
+            public void changedUpdate(DocumentEvent e) { validateAmount(); }
+            public void removeUpdate(DocumentEvent e) { validateAmount(); }
+            public void insertUpdate(DocumentEvent e) { validateAmount(); }
+        });
 
-        buyerList.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        buyerList.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Select a buyer", "Item 2", "Item 3", "Item 4" }));
 
-        jLabel5.setText("Payback :");
-
-        paybackCheckBox.setText("jCheckBox1");
-
-        jLabel7.setText("Amount :");
+        paybackCheckBox.setText("Payback");
 
         jScrollPane1.setName("Debts"); // NOI18N
 
@@ -96,60 +113,47 @@ public class AddTransactionDialog extends javax.swing.JDialog {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel2)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(storeTextBox, javax.swing.GroupLayout.PREFERRED_SIZE, 128, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(amountTextBox, javax.swing.GroupLayout.PREFERRED_SIZE, 128, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(receiptTextBox)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel3)
-                            .addComponent(jLabel1)
-                            .addComponent(jLabel4)
-                            .addComponent(jLabel5)
-                            .addComponent(jLabel7))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(paybackCheckBox)
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                .addComponent(storeTextBox, javax.swing.GroupLayout.Alignment.LEADING)
-                                .addComponent(receiptTextBox, javax.swing.GroupLayout.Alignment.LEADING)
-                                .addComponent(amountTextBox, javax.swing.GroupLayout.Alignment.LEADING)
-                                .addComponent(buyerList, javax.swing.GroupLayout.Alignment.LEADING, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(buyerList, javax.swing.GroupLayout.PREFERRED_SIZE, 127, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(jLabel2))
+                            .addComponent(paybackCheckBox))
+                        .addGap(0, 0, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(saveButton)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(cancelButton)))
-                .addContainerGap(141, Short.MAX_VALUE))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
-                    .addComponent(storeTextBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jLabel2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                    .addComponent(storeTextBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(amountTextBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(receiptTextBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel3)
-                    .addComponent(receiptTextBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(amountTextBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel7))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel4)
-                    .addComponent(buyerList, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel5)
-                    .addComponent(paybackCheckBox))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 227, Short.MAX_VALUE)
+                    .addComponent(buyerList, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(paybackCheckBox)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 304, Short.MAX_VALUE)
                 .addGap(7, 7, 7)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(saveButton)
                     .addComponent(cancelButton))
-                .addContainerGap())
+                .addGap(10, 10, 10))
         );
 
         pack();
@@ -170,16 +174,31 @@ public class AddTransactionDialog extends javax.swing.JDialog {
     private javax.swing.JTextField amountTextBox;
     private javax.swing.JComboBox buyerList;
     private javax.swing.JButton cancelButton;
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel7;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JCheckBox paybackCheckBox;
     private javax.swing.JTextField receiptTextBox;
     private javax.swing.JButton saveButton;
     private javax.swing.JTextField storeTextBox;
     // End of variables declaration//GEN-END:variables
+
+    
+    private void validateStore() {
+        if (storeTextBox.getText().isEmpty())
+            storeTextBox.setBorder(invalidTextFieldBorder);
+        else storeTextBox.setBorder(defaultTextFieldBorder);
+    }
+    private void validateReceipt() {
+        if (receiptTextBox.getText().isEmpty())
+            receiptTextBox.setBorder(invalidTextFieldBorder);
+        else receiptTextBox.setBorder(defaultTextFieldBorder);
+    }
+    private void validateAmount() {
+        try {
+            Float.parseFloat(amountTextBox.getText());
+            amountTextBox.setBorder(defaultTextFieldBorder);
+        } catch (NumberFormatException e) {
+            amountTextBox.setBorder(invalidTextFieldBorder);
+        }
+    }
 }
