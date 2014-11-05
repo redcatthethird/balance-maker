a32f3a9473d600ac0abb0a3f1ad1f66c06fc16a7
Red Cat <cred@gmx.us>
2014-11-05 12:58:31 +0000

    Added InstanceManager class.

--- src/balancemaker/InstanceManager.java
+++ src/balancemaker/InstanceManager.java
@@ -5,10 +5,43 @@
  */
 package balancemaker;
 
+import java.lang.ref.*;
+import java.util.*;
+
 /**
  *
  * @author Red
  */
-public class InstanceManager {
-    // TODO: Implement functionality from Radiodef's answer on SE
+public final class InstanceManager<T> {
+    private final Map<Class<?>, List<Reference<T>>> refMap = new HashMap<>();
+    
+    public synchronized <U extends T> U manage(U instance) {
+        Class<?> cls = instance.getClass();
+        List<Reference<T>> refList = refMap.get(cls);
+        
+        if (refList == null) {
+            refList = new LinkedList<>();
+            refMap.put(cls, refList);
+        }
+        
+        refList.add(new WeakReference<>(instance));
+        return instance;
+    }
+    
+    public synchronized <U extends T> List<U> getAll(Class<U> cls) {
+        List<U> returnList = new LinkedList<>();
+        
+        List<Reference<T>> refList = refMap.get(cls);
+        if (refList != null) {
+            Iterator<Reference<T>> it = refList.iterator();
+            while (it.hasNext()) {
+                T instance = it.next().get();
+                
+                if (instance == null) it.remove();
+                else returnList.add(cls.cast(instance));
+            }
+        }
+        
+        return returnList;
+    }
 }
